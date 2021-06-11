1c2c74e69b8bacf8bdbf10d2295125c0b635fdee
Vladusx64 <vladus032@gmail.com>
2021-06-12 01:19:50 +0300

    fix bug

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,12 +4,6 @@ date_default_timezone_set('Europe/Kiev');
 
 ini_set('display_errors', true);
 
-
-define('MODELS_DIR', __DIR__ . '/models/');
-define('CONTROLLERS_DIR', __DIR__ . '/controllers/');
-define('VIEWS_DIR', __DIR__ . '/views/');
-define('STORE_DIR', __DIR__ . '/store/');
-
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/config.php';
 
