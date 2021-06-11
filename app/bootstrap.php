e0a1dbff030b91f70f0f5f2c648ec3917006a8f4
Vladusx64 <vladus032@gmail.com>
2021-06-12 01:04:23 +0300

    view tamplate

---
+++ app/bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+
+date_default_timezone_set('Europe/Kiev');
+
+ini_set('display_errors', true);
+
+
+define('MODELS_DIR', __DIR__ . '/models/');
+define('CONTROLLERS_DIR', __DIR__ . '/controllers/');
+define('VIEWS_DIR', __DIR__ . '/views/');
+define('STORE_DIR', __DIR__ . '/store/');
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use App\Controllers\PictureController;
+
+$controllerObj = new PictureController();
+
---
+++ app/controllers/PictureController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controllers;
+
+class PictureController
+{
+    public function __construct()
+    {
+        require_once VIEWS_DIR .  'main.php';
+    }
+}
